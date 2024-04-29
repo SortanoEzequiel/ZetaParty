eab4487abad9275dcf40ca8fb271c375ac348855
SortanoEzequiel <sortano59@gmail.com>
2024-04-28 22:38:37 -0300

    commit

--- front/src/componentes/nav.jsx
+++ front/src/componentes/nav.jsx
@@ -59,7 +59,7 @@ const Navbartres = () => {
                 </div>
               </MenuItemLink>
             </MenuItem>
-            { isAuthenticated && user.name === NAME ? (
+            { isAuthenticated && user.name === 'Ezequiel Sortano' ? (
                     <MenuItem>
                     <MenuItemLink onClick={() => setShowMobileMenu(!showMobileMenu)}><div>
                                        
