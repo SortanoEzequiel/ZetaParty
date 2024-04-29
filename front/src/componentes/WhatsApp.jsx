894f2134a1560edce8e0cf98b0d131aa4b6d09bd
SortanoEzequiel <sortano59@gmail.com>
2024-04-29 20:58:35 -0300

    commi

--- front/src/componentes/WhatsApp.jsx
+++ front/src/componentes/WhatsApp.jsx
@@ -5,7 +5,7 @@ import style from "./whatsapp.module.css";
 export default function WhatsApp (){
     return(
         <div className = {style.container}>
-        <a href='https://api.whatsapp.com/send?phone=0111555663291' target="_blank">
+        <a href='https://api.whatsapp.com/send?phone=111555663291' target="_blank">
             <img src={logo} alt="" />
         </a>
         </div>
